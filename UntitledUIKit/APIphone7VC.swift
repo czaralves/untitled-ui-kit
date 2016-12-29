008379d1adcf7493f15585db0260b7b34e5cd82c
Cesar Alves <Cesar@mac7950>
2016-12-29 17:41:39 -0200

    Initial commit

---
+++ UntitledUIKit/APIphone7VC.swift
@@ -0,0 +1,17 @@
+//
+//  APIphone7VC.swift
+//  Untitled
+//
+//  Created by czaralves@gmail.com on 29/12/2016, using AnimaApp.com, under MIT license.
+//  Copyright © 2016 Company Name. All rights reserved.
+//
+
+import UIKit
+
+public class APIphone7VC : ANViewController {
+    
+    @IBOutlet weak var rectangle: UIView?
+
+    
+
+}
---
+++ UntitledUIKit/UntitledUIKit.h
@@ -0,0 +1,39 @@
+//
+//  UntitledUIKit.h
+//  UntitledUIKit
+//
+//  Created by czaralves@gmail.com on 29/12/2016, using AnimaApp.com, under MIT license.
+//  Copyright © 2016 Company Name. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UntitledUIKit.
+FOUNDATION_EXPORT double UntitledUIKitVersionNumber;
+
+//! Project version string for UntitledUIKit.
+FOUNDATION_EXPORT const unsigned char UntitledUIKitVersionString[];
+
+#import <UntitledUIKit/ANVideoPlayer.h>
+#import <UntitledUIKit/UIImageView+Anima.h>
+#import <UntitledUIKit/ANProtocols.h>
+#import <UntitledUIKit/ANLabel.h>
+#import <UntitledUIKit/ANClassMapping.h>
+#import <UntitledUIKit/NSString+Anima.h>
+#import <UntitledUIKit/ANTextField.h>
+#import <UntitledUIKit/UIView+Anima.h>
+#import <UntitledUIKit/ANCSVParser.h>
+#import <UntitledUIKit/ANNavigationController.h>
+#import <UntitledUIKit/AnimaSDK.h>
+#import <UntitledUIKit/Anima-Bridging-Header.h>
+#import <UntitledUIKit/UIView+AnimaConstraints.h>
+#import <UntitledUIKit/ANGradientView.h>
+#import <UntitledUIKit/ANTabBarController.h>
+#import <UntitledUIKit/MKMapView+Anima.h>
+#import <UntitledUIKit/ANMacros.h>
+#import <UntitledUIKit/NSDictionary+Anima.h>
+#import <UntitledUIKit/ANViewController.h>
+#import <UntitledUIKit/UIWebView+Anima.h>
+#import <UntitledUIKit/ANRSSParser.h>
+#import <UntitledUIKit/ANTheme.h>
+#import <UntitledUIKit/ANDataList.h>
